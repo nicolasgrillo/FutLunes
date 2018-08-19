d66f56e18e3878e6dd5d23cdfbe7aade86ec75ce
Nicolas Grillo <nicolasgrillo@live.com.ar>
2018-08-19 18:24:12 -0300

    Add match method

--- src/providers/match-service/match-service.ts
+++ src/providers/match-service/match-service.ts
@@ -2,16 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment as ENV } from '../../environments/environment';
 import { Observable } from '../../../node_modules/rxjs/Observable';
+import { CreateMatchModel } from '../../models/CreateMatchModel';
 
 @Injectable()
 export class MatchServiceProvider {
 
   private baseApiUrl = ENV.API_BASE_URL;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient
+  ) {}
 
   public getCurrentMatch(): Observable<any>{
     return this.http.get(this.baseApiUrl + "api/matches/current");
+  }
+
+  public addMatch(cmm : CreateMatchModel, token : string) : Observable<any> {
+    return this.http.post(
+      this.baseApiUrl + "api/matches/add",
+      JSON.stringify(cmm),
+      {
+        headers: {
+          'Content-Type':'application/json',
+          'Authorization':'Bearer ' + token
+        }
+      }
+    ) 
   }
 
 }
